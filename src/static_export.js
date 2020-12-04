9d50529078adbce5a003ed158c4e7520577c470a
Parissa <parissa.eggleston@gmail.com>
2020-12-04 12:19:03 -0500

    initial load of razzel hello world

---
+++ src/static_export.js
@@ -0,0 +1,9 @@
+// ./src/static_export.js
+import { renderApp } from './server';
+export const render = (req, res) => {
+  const { html } = renderApp(req, res);
+  res.json({ html });
+};
+export const routes = () => {
+  return ['/'];
+};
